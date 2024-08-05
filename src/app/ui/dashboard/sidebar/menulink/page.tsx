818f1e69b587b18782bbebb4120debffe270cc0b
Sanguine Feranmi <feranmibayeri@gmail.com>
2024-08-05 17:00:10 +0100

    sixth commit

--- src/app/ui/dashboard/sidebar/menulink/page.tsx
+++ src/app/ui/dashboard/sidebar/menulink/page.tsx
@@ -3,28 +3,29 @@
 // import styles from './menulink.module.css';
 // import { usePathname } from 'next/navigation';
 
-// interface MenuItem {
+// interface  MenuItem {
 //   title: string;
 //   path: string;
 //   Icon: JSX.Element;
 // }
 
-// const Menulink = ({ item }: { item: MenuItem }) => {
-//     const pathname = usePathname()
-    
+// const Menulinks = ({ item }: { item: MenuItem}) => {
+//   const pathname = usePathname();
+
 //   return (
-//     // <Link href={item.path} passHref className={'${styles.container} ${pathname === item.path && styles.active}'}>
 //     <Link href={item.path} passHref className={`${styles.container} ${pathname === item.path ? styles.active : ''}`}>
-//       {/* <a className={styles.container}> */}
-//         {item.Icon}
-//         {item.title}
-//       {/* </a> */}
+//       {item.Icon}
+//       {item.title}
 //     </Link>
 //   );
 // };
-// export default Menulink;
 
-"use client"
+// export default Menulinks;
+
+
+
+"use client";
+
 import Link from 'next/link';
 import styles from './menulink.module.css';
 import { usePathname } from 'next/navigation';
@@ -35,7 +36,11 @@ interface MenuItem {
   Icon: JSX.Element;
 }
 
-const Menulinks = ({ item }: { item: MenuItem}) => {
+interface MenulinksProps {
+  item: MenuItem;
+}
+
+const Menulinks: React.FC<MenulinksProps> = ({ item }) => {
   const pathname = usePathname();
 
   return (
