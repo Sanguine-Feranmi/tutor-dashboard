5700f4981152575ecfe27bcda52bddc0cf2d62f9
Sanguine Feranmi <feranmibayeri@gmail.com>
2024-08-05 17:10:23 +0100

    menulink fix

--- src/app/ui/dashboard/sidebar/menulink/page.tsx
+++ src/app/ui/dashboard/sidebar/menulink/page.tsx
@@ -24,11 +24,42 @@
 
 
 
+// "use client";
+
+// import Link from 'next/link';
+// import styles from './menulink.module.css';
+// import { usePathname } from 'next/navigation';
+
+// interface MenuItem {
+//   title: string;
+//   path: string;
+//   Icon: JSX.Element;
+// }
+
+// interface MenulinksProps {
+//   item: MenuItem;
+// }
+
+// const Menulinks: React.FC<MenulinksProps> = ({ item }) => {
+//   const pathname = usePathname();
+
+//   return (
+//     <Link href={item.path} passHref className={`${styles.container} ${pathname === item.path ? styles.active : ''}`}>
+//       {item.Icon}
+//       {item.title}
+//     </Link>
+//   );
+// };
+
+// export default Menulinks;
+
+
 "use client";
 
 import Link from 'next/link';
 import styles from './menulink.module.css';
 import { usePathname } from 'next/navigation';
+import { FC } from 'react';
 
 interface MenuItem {
   title: string;
@@ -40,7 +71,7 @@ interface MenulinksProps {
   item: MenuItem;
 }
 
-const Menulinks: React.FC<MenulinksProps> = ({ item }) => {
+const Menulinks: FC<MenulinksProps> = ({ item }) => {
   const pathname = usePathname();
 
   return (
