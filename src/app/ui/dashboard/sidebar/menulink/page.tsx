7560d7116c8e87314db7d2ecbeeef8f333020fde
Sanguine Feranmi <feranmibayeri@gmail.com>
2024-08-05 02:13:56 +0100

    third commit

--- src/app/ui/dashboard/sidebar/menulink/page.tsx
+++ src/app/ui/dashboard/sidebar/menulink/page.tsx
@@ -1,3 +1,29 @@
+// "use client"
+// import Link from 'next/link';
+// import styles from './menulink.module.css';
+// import { usePathname } from 'next/navigation';
+
+// interface MenuItem {
+//   title: string;
+//   path: string;
+//   Icon: JSX.Element;
+// }
+
+// const Menulink = ({ item }: { item: MenuItem }) => {
+//     const pathname = usePathname()
+    
+//   return (
+//     // <Link href={item.path} passHref className={'${styles.container} ${pathname === item.path && styles.active}'}>
+//     <Link href={item.path} passHref className={`${styles.container} ${pathname === item.path ? styles.active : ''}`}>
+//       {/* <a className={styles.container}> */}
+//         {item.Icon}
+//         {item.title}
+//       {/* </a> */}
+//     </Link>
+//   );
+// };
+// export default Menulink;
+
 "use client"
 import Link from 'next/link';
 import styles from './menulink.module.css';
@@ -10,16 +36,14 @@ interface MenuItem {
 }
 
 const Menulink = ({ item }: { item: MenuItem }) => {
-    const pathname = usePathname()
-    
+  const pathname = usePathname();
+
   return (
-    // <Link href={item.path} passHref className={'${styles.container} ${pathname === item.path && styles.active}'}>
     <Link href={item.path} passHref className={`${styles.container} ${pathname === item.path ? styles.active : ''}`}>
-      {/* <a className={styles.container}> */}
-        {item.Icon}
-        {item.title}
-      {/* </a> */}
+      {item.Icon}
+      {item.title}
     </Link>
   );
 };
+
 export default Menulink;
