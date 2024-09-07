ec83d42a6b07480d04155796882f15234926c833
Sanguine Feranmi <feranmibayeri@gmail.com>
2024-09-07 05:00:17 +0100

    right bar

--- src/app/ui/dashboard/rightbar/page.tsx
+++ src/app/ui/dashboard/rightbar/page.tsx
@@ -1,7 +1,24 @@
+import { MdArrowDropDown } from "react-icons/md"
+import styles from "./rightbar.module.css"
+
 const Rightbar  =()=>{
     return(
-        <div>
-        Rightbar
+        <div className={styles.right}>
+            <div className={styles.rightHead}>
+                <p>You have Upcoming Events</p>
+                <hr />
+            </div>
+            <div className={styles.reminder}>
+                <p>Reminder</p>
+            </div>
+            <div className={styles.events}>
+                <div className={styles.eventsHead}>
+                    <p>Events</p>
+                    <p>
+                        <MdArrowDropDown/>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
